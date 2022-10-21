47cc954fc38c411955a91058dbb3e0ab20ef29c2
Serge <sergeykonstwork@gmail.com>
2022-10-21 13:57:42 +0300

    roles

---
+++ practice 2-5/main/roles and rights.sql
@@ -0,0 +1,14 @@
+SET SCHEMA 'little_company';
+
+--Создание груповых ролей, которым будут выдавать привилегии в соответствии с ТЗ, 
+--а в них уже добавляться наши рядовые работники и менеджеры.
+CREATE ROLE worker;
+CREATE ROLE manager;
+
+--Создание пользователей работников компании
+CREATE ROLE Artem LOGIN PASSWORD 'little_artemius';
+CREATE ROLE Dmitry LOGIN PASSWORD 'little_dmitry';
+CREATE ROLE Sergey LOGIN PASSWORD 'little_sergey';
+
+GRANT worker TO Artem, Dmitry;
+GRANT manager TO Sergey;
