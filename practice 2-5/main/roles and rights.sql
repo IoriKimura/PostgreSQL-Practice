172a16ab9bbb523e0c650ee6ee0908d4aeb495b0
Serge <sergeykonstwork@gmail.com>
2022-10-23 15:05:53 +0300

    Правки привилегий
    Изменений в привилегиях ролей работник и менеджер.

--- practice 2-5/main/roles and rights.sql
+++ practice 2-5/main/roles and rights.sql
@@ -14,5 +14,40 @@ GRANT worker TO Artem, Dmitry;
 GRANT manager TO Sergey;
 
 GRANT SELECT, INSERT, UPDATE ON tasks TO manager;
+GRANT SELECT, INSERT, UPDATE ON contracts to manager;
 GRANT SELECT, UPDATE ON tasks TO worker;
 
+ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY worker_select ON tasks FOR SELECT TO worker USING
+(
+	(SELECT employee_id FROM employee WHERE 'position' = 'worker'
+	AND
+	(SELECT rolname FROM pg_roles WHERE pg_has_role(current_user, 'mamber') AND rolname = 'worker') = 'worker'
+	) = tasks.executor_id
+);
+
+CREATE POLICY worker_update ON tasks FOR UPDATE TO worker USING
+(
+	(SELECT employee_id FROM employee WHERE 'position' = 'worker'
+	AND
+	(SELECT rolname FROM pg_roles WHERE pg_has_role(current_user, 'mamber') AND rolname = 'worker') = 'worker'
+	) = tasks.executor_id
+);
+
+CREATE POLICY manager_update ON tasks FOR UPDATE TO  manager USING
+(
+	(SELECT employee_id FROM employee WHERE 'position' = 'manager'
+	AND
+	(SELECT rolname FROM pg_roles WHERE pg_has_role(current_user, 'mamber') AND rolname = 'manager') = 'manager'
+	) = tasks.author_id 
+	OR 
+	(SELECT employee_id FROM employee WHERE 'position' = 'manager'
+	AND
+	(SELECT rolname FROM pg_roles WHERE pg_has_role(current_user, 'mamber') AND rolname = 'manager') = 'manager'
+	) = tasks.executor_id
+);
+
+
+
+--SELECT rolname FROM pg_roles WHERE pg_has_role(user, 'member') and rolname = 'manager';
