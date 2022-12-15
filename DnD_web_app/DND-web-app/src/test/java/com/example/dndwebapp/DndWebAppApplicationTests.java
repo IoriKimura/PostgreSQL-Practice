9ce73db9f2f187bd2646c46b74639a643d1ff1bd
Serge <sergeykonstwork@gmail.com>
2022-12-15 15:35:20 +0300

    Создание проекта, заполнение таблиц
    Создание проекта для дальнейшей работы
    Скрипты, по которым заполнялись таблицы
    
    Co-Authored-By: 4rutemu <98741614+4rutemu@users.noreply.github.com>

---
+++ DnD_web_app/DND-web-app/src/test/java/com/example/dndwebapp/DndWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dndwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
