2120ae1a0da55f79e017091137ff37540fd32b07
Serge <sergeykonstwork@gmail.com>
2022-12-20 14:33:35 +0300

    upd

--- DnD_web_app/scripts/functions.sql
+++ DnD_web_app/scripts/functions.sql
@@ -9,6 +9,14 @@ CREATE PROCEDURE saveNewUser (TEXT, TEXT, TEXT, TEXT) AS
 	'INSERT INTO public.users(user_name, nick_name, email, password) VALUES($1, $2, $3, $4);'
 language SQL;
 
+CREATE PROCEDURE savenewcharacter (INTEGER, INTEGER, INTEGER, INTEGER, INTEGER, INTEGER, TEXT) AS
+	'INSERT INTO public.characters(user_id, class_id, race_id, subrace_id,
+	weapon_id, armour_id, character_name) VALUES($1, $2, $3, $4, $5, $6, $7);'
+language SQL;
+
+
+
+
 CREATE FUNCTION findCharactersByUserId (INTEGER)
 RETURNS TABLE ("id" INTEGER, 
 			   character_name TEXT,
